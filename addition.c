4b607e14d3fe76f3b14c7b8fa0ea7851085f0cd3
138abhinava-d <110719525+138abhinava-d@users.noreply.github.com>
2022-08-06 17:30:51 +0530

    Update addition.c
    added 4 numbers using user defined function

--- addition.c
+++ addition.c
@@ -1,11 +1,16 @@
 // to add two numbers//
 #include<stdio.h>
-int main()
+int add( int a, int b, int c, int d)
 {
- int a, b ,sum;
-  printf("enter the valuue of the two numbers which have to be added: ");
-    scanf("%d%d ", &a, &b);
-  sum=a+b;
+ int sum = a+b+c+d;
+ return sum ;
+}
+int main() 
+{
+ int a, b , c, d,sum;
+  printf("enter the value of thefour numbers which have to be added: ");
+  scanf("%d%d%d%d", &a, &b, &c, &d);
+  sum=add(a,b,c,d);
   printf("the sum of thetwo numbers which you have entered is= %d ", sum);
   return 0;
 }
