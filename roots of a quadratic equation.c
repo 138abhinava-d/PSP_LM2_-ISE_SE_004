54568009871dd3a8d9a83c9d82747b47d7e90f3b
138abhinava-d <110719525+138abhinava-d@users.noreply.github.com>
2022-08-11 23:10:58 +0530

    completed successfully

--- roots of a quadratic equation.c
+++ roots of a quadratic equation.c
@@ -2,24 +2,17 @@
 #include<math.h>
 void main()
 {
-  float a, b, c, d, root1, root2;
+  float a, b, c, d, r1, r2;
   printf("enter the coefficient of the terms of the quadratic eqn.: \n");
   scanf("%f%f%f", &a,&b,&c);
   if(a==0||b==0||c==0)
-    printf("invalid statement");
-  else()
+    printf("invalid input!");
+  else
   {
-    d=b*b-4ac;
+    d=b*b-4*a*c;
     if(d>0)
-      printf(" the roots are real and distinct \n"); 
-      r1=(-b+sqrt(d)/(2*a);
-      r2=(-b-sqrt(d)/(2*a);
-      printf("r1=%f",r1);
-      printf("r2=%f",r2);
-      }
-      else if(d==0)
-      {
-        printf("the roots ARE REAL AND DISTINCT \n");
+    {
+     printf("the roots are real and distinct \n");
         r1=(-b+sqrt(d))/(2*a);
         r2=(-b+sqrt(d))/(2*a);
         printf("r1=%f \n",r1);
@@ -28,10 +21,16 @@ void main()
           else if(d==0)
       {
      printf("the roots are real and equal");
-     r1=r2=-b(2*a);
+     r1=r2=-b/(2*a);
      printf("\n r1=r2=%f", r1);
       }
           else
           {
             printf("the roots are complex");
-            
+            r1=-b/(2*a);
+            r2=sqrt(fabs(d))/(2*a);
+            printf("\n r1=%f+i%f",r1, r2);
+            printf("\n r2=%f+i%f",r1,r2);
+          }
+          }
+}
